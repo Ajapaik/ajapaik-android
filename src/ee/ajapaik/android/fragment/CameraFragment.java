dd2800e0b936ff9df5fb04cc0e5511920d719031
Madis Pink <madis.pink@mobisolutions.com>
2012-10-06 20:35:29 +0300

    WIP camera stuff

--- src/ee/ajapaik/android/fragment/CameraFragment.java
+++ src/ee/ajapaik/android/fragment/CameraFragment.java
@@ -1,15 +1,25 @@
 package ee.ajapaik.android.fragment;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Surface;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
@@ -18,20 +28,35 @@ import com.example.touch.TouchImageView;
 import ee.ajapaik.android.R;
 import ee.ajapaik.android.camera.CameraPreview;
 
-public class CameraFragment extends Fragment {
+public class CameraFragment extends Fragment implements Camera.ShutterCallback, Camera.PictureCallback {
 
 	public static String TAG = "CameraFragment";
 	private Camera camera;
 	private FrameLayout previewSurfaceContainer;
+	private SimpleDateFormat format;
 
 	public static Fragment newInstance() {
 		return new CameraFragment();
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		format = new SimpleDateFormat();
+		format.applyPattern("dM_kms");
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup vg, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.camera, null);
 		previewSurfaceContainer = (FrameLayout) v.findViewById(R.id.cameraPreview);
+		v.findViewById(R.id.photoButton).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				camera.takePicture(CameraFragment.this, null, CameraFragment.this);
+			}
+		});
 		return v;
 	}
 
@@ -124,6 +149,43 @@ public class CameraFragment extends Fragment {
 			result = (info.orientation - degrees + 360) % 360;
 		}
 		camera.setDisplayOrientation(result);
+	}
+
+	@Override
+	public void onPictureTaken(byte[] data, Camera camera) {
+		String filename = format.format(new Date());
+		File f = new File(Environment.DIRECTORY_PICTURES + File.separator + "ajapaik");
+		if (f.exists()) {
+			Log.w(TAG, "Going to overwrite picture at " + filename);
+		}
+		f.mkdirs();
+		FileOutputStream fos = null;
+		BufferedOutputStream bos = null;
+		try {
+			fos = new FileOutputStream(f.getAbsolutePath() + File.separator + filename + ".jpg");
+			bos = new BufferedOutputStream(fos, 8096); // 8K buffer
+			
+			int totalWritten = 0;
+			int written = 0;
+			while (bos.write(data))
+		} catch (Exception e) {
+			Log.w(TAG, "Saving file failed :(");
+		} finally {
+			if (fos != null) {
+				try {
+					fos.close();
+				} catch (IOException ioe) {}
+				try {
+					bos.close();
+				} catch (IOException ioe) {}
+				}
+			}
+		}
+	}
+
+	@Override
+	public void onShutter() {
+		// make the shutter sound
 	}
 
 }
